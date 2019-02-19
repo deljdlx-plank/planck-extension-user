0ef01d1adfcaf8a99b15235554b93de4f130411e
Jean luc <you@example.com>
2019-02-19 17:10:35 +0100

    synchro

--- source/class/Model/Descriptor/User.php
+++ source/class/Model/Descriptor/User.php
@@ -4,14 +4,19 @@ namespace Planck\Extension\User\Model\Descriptor;
 
 
 use Planck\Model\EntityDescriptor;
+use Planck\Model\Repository;
 
 class User extends EntityDescriptor
 {
 
-    public function getLabelFieldName()
+
+    public function __construct(Repository $repository, array $descriptor = null)
     {
-        return 'login';
+        parent::__construct($repository, $descriptor);
+        $this->getFieldByName('login')->isLabel(true);
     }
+
+
 
 
 }
